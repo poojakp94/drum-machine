29eea59e89af867e1c71855fe70cc924fbe0f94f
poojakp94 <poojakprasad94@gmail.com>
2020-09-22 21:36:22 +0530

    Updates

---
+++ precache-manifest.a3a2d8bca9dfe574b2d17a280ac0ff69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05ec7094719d6ca7c8ee1130cb60b94",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "29921a58533f019c1fe1",
+    "url": "/drum-machine/static/css/main.ea999bb0.chunk.css"
+  },
+  {
+    "revision": "3bdec051e0b7fca697a4",
+    "url": "/drum-machine/static/js/2.e0b2333e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.e0b2333e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29921a58533f019c1fe1",
+    "url": "/drum-machine/static/js/main.ad3e5253.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  }
+]);
